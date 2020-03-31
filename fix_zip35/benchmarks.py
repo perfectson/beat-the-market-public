cbe384da2012b09f0c1cafb79c17b6857e390158
perfectson <perfectson@hotmail.it>
2020-03-31 10:57:49 +0000

    fixing zipline

---
+++ fix_zip35/benchmarks.py
@@ -0,0 +1,48 @@
+#
+# Copyright 2013 Quantopian, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+	
+import pandas as pd
+from datetime import datetime
+from trading_calendars import get_calendar
+def get_benchmark_returns(symbol):
+    cal = get_calendar('NYSE')
+    first_date = datetime(1930,1,1)
+    last_date = datetime(2030,1,1)
+    dates = cal.sessions_in_range(first_date, last_date)
+    data = pd.DataFrame(0.0, index=dates, columns=['close'])
+    data = data['close']    
+    return data.sort_index().iloc[1:]
+
+
+
+
+
+
+
+
+
+
+
+
+xccxcxzcx    
+
+
+
+
+
+
+
+
